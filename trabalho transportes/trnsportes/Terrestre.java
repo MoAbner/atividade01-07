ad759d394c256eedf57aa9a1825491986ab0a463
MoAbner <153506543+MoAbner@users.noreply.github.com>
2024-07-01 20:19:14 -0300

    Add files via upload

---
+++ trabalho transportes/trnsportes/Terrestre.java
@@ -0,0 +1,12 @@
+public abstract class Terrestre extends Transporte {
+    private int numDeRodas;
+
+    public Terrestre(int capacidade, int velocidade, int numDeRodas) {
+        super(capacidade, velocidade);
+        this.numDeRodas = numDeRodas;
+    }
+
+    public int getNumDeRodas() {
+        return numDeRodas;
+    }
+}
